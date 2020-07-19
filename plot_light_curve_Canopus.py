ef3407e4603b64ef258c0273f92cbd5232de547b
rosahoogenboom <68503682+rosahoogenboom@users.noreply.github.com>
2020-07-19 15:40:57 +0200

    Add files via upload

---
+++ plot_light_curve_Canopus.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pyfits as pf
+
+#change for different stars
+camera = "SAE"
+title_star = "Canopus"
+star = 2111805 #ASCC number
+
+#import calibrated light curve taken by the South African eastern camera
+filename = '/data2/sanna/FitsSAE/2111805SAE_detrended.fits'
+data = pf.getdata(filename)
+
+#plot the light curve
+plt.plot(data['jd'], data['mag0'], '.', alpha = 0.1)
+plt.ylabel("Magnitude")
+plt.xlabel("Time (Julian Date)")
+plt.title(str(title_star) + " (camera " + str(camera) +")")
+plt.gca().invert_yaxis()
+plt.show()
